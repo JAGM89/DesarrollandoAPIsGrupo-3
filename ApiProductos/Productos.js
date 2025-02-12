01af3de57645168684d21979fd77099e3fa74d71
Brayan Argenal <argenalbrian4@gmail.com>
2025-02-11 20:05:13 -0600

    Add/ApiProducto
    Se suma el ejercicio de la api producto

---
+++ ApiProductos/Productos.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req,res) => {
+    res.send('[{"id" : 1, "nombre":"Laptop","precio":1200}]');
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
