53d0d292021b3c98c0b5c8ea732486084d5896c5
Brayan Argenal <argenalbrian4@gmail.com>
2025-02-12 19:19:13 -0600

    ApiFixed
    Actualizacion 1.2

--- ApiProductos/Productos.js
+++ ApiProductos/Productos.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req,res) => {
-    res.json('[{"id" : 1, "nombre":"Laptop","precio":1200}]');
+app.get('/', (req, res) => {
+    res.json([{ "id": 1, "nombre": "Laptop", "precio": 1200 }]);
 });
 
 
